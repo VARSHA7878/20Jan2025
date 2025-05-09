a29df2f72f886738cc27df78d79dce890332f302
varsha <varshabinwade@gmail.com>
2025-05-09 11:45:35 +0530

    added project

---
+++ TestJan2025Project/src/test/java/testPackage/Test1.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	
+	@Test
+	public void a()
+	{
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		//System.out.println("Hello testNG");
+		driver.get("https://mvnrepository.com/");
+	}
+}
